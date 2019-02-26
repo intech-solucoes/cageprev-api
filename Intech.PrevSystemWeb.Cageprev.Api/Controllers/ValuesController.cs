8c4983d3ed327c82792a861f570671c4586cb821
Rony <ronymmoura@gmail.com>
2019-02-26 20:39:46 -0300

    Criando estrutura

---
+++ Intech.PrevSystemWeb.Cageprev.Api/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿#region Usings
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection; 
+#endregion
+
+namespace Intech.PrevSystemWeb.Cageprev.Api.Controllers
+{
+    [Route("api/")]
+    public class VersaoController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName();
+            return Json(version.Version.ToString(3));
+        }
+    }
+}

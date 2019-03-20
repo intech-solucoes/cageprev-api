510cb535e2f60c705db3e29094b6967096fbbc1a
Rony <ronymmoura@gmail.com>
2019-03-20 19:25:15 -0300

    Criando rota de ficha financeira de assistidos

--- Intech.PrevSystemWeb.Cageprev.Api/Controllers/FIchaFinancAssistidoController.cs
+++ Intech.PrevSystemWeb.Cageprev.Api/Controllers/FIchaFinancAssistidoController.cs
@@ -3,22 +3,55 @@ using Intech.PrevSystemWeb.Api;
 using Intech.PrevSystemWeb.Negocio.Proxy;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System; 
+using System;
+using System.Globalization;
 #endregion
 
 namespace Intech.PrevSystemWeb.Cageprev.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class FIchaFinancAssistidoController : BaseController
+    public class FichaFinancAssistidoController : BaseController
     {
-        [HttpGet("porProcesso/{sqProcesso}")]
+        [HttpGet("ultimaPorProcesso/{sqProcesso}")]
         [Authorize("Bearer")]
-        public IActionResult GetPorPlano(int sqProcesso)
+        public IActionResult GetUltimaPorProcesso(int sqProcesso)
         {
             try
             {
-                return Json(new FichaFinancAssistidoProxy().BuscarPorProcesso(sqProcesso));
+                return Json(new FichaFinancAssistidoProxy().BuscarUltimaPorProcesso(sqProcesso));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("datasPorProcesso/{sqProcesso}")]
+        [Authorize("Bearer")]
+        public IActionResult GetDatasPorProcesso(int sqProcesso)
+        {
+            try
+            {
+                var quantidadeMesesContraCheque = 18;
+                var dtReferencia = DateTime.Today.PrimeiroDiaDoMes().AddMonths(-quantidadeMesesContraCheque);
+
+                return Json(new FichaFinancAssistidoProxy().BuscarDatasPorProcesso(sqProcesso, dtReferencia));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("porProcessoCompetencia/{sqProcesso}/{competencia}")]
+        [Authorize("Bearer")]
+        public IActionResult GetPorPlano(int sqProcesso, string competencia)
+        {
+            try
+            {
+                var dtCompetencia = DateTime.ParseExact(competencia, "dd.MM.yyyy", new CultureInfo("pt-BR"));
+                return Json(new FichaFinancAssistidoProxy().BuscarRubricasPorProcessoCompetencia(sqProcesso, dtCompetencia));
             }
             catch (Exception ex)
             {
